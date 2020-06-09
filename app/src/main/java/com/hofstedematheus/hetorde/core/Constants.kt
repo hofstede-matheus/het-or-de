019bd281a37cc29a1408606b18848c954ae3461b
hofstede-matheus <matheus.hofstede@cubos.io>
2020-06-08 21:27:06 -0300

    feat: implement ads Closes #8

---
+++ app/src/main/java/com/hofstedematheus/hetorde/core/Constants.kt
@@ -0,0 +1,6 @@
+package com.hofstedematheus.hetorde.core
+
+object Constants {
+    const val ADMOB_APP_ID = "ca-app-pub-4216914448905685~8701754661"
+    const val ADMOB_FOOTER_BANNER_AD_ID = "ca-app-pub-4216914448905685/1692319724"
+}
